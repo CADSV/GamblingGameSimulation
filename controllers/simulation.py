b54beb81ac963cb92eb57593d429f35e886834a3
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-06-30 22:40:25 -0400

    Agregado el ejercicio bonus

--- controllers/simulation.py
+++ controllers/simulation.py
@@ -1,9 +1,13 @@
 #Needed imports
+from fileinput import filename
 from views.utils import (
     clear_screen, #To clear the screen
 )
 
 import random
+import numpy as np
+import pandas as pd
+from datetime import datetime
 
 
 #Function of the simulations
@@ -17,6 +21,7 @@ def simulation(context: dict, bonus: int) -> dict:
     rounds = int(input('Ingrese el número de rondas a jugar: '))
     plays = []
     consecutiveLosses = 0
+    consecutiveWins = 0
     numBets = 0
     simulation = []
     bet = initialBet
@@ -27,36 +32,98 @@ def simulation(context: dict, bonus: int) -> dict:
         while 0 < initialAmount < target:
             plays.append({
                 'monto_antes': initialAmount,
-                'bet': (bet := min(bet, initialAmount)),
+                'apuesta': (bet := min(bet, initialAmount)),
                 'rand': (rand := random.uniform(0,1)),
                 'victoria': (victory := rand < 0.5),
                 'nuevo_monto': (initialAmount := min(target, initialAmount + bet) if victory else max(initialAmount - bet, 0)),
             })
             print(plays)
+            print('\n')
             input()
-            consecutiveLosses = 0 if victory else consecutiveLosses + 1
-            bet = initialBet if victory else bet * 2 ** consecutiveLosses
+
+            if(bonus):
+                consecutiveWins = consecutiveWins + 1 if victory else 0
+                bet = bet * 2  if victory else initialBet
+            else:
+                consecutiveLosses = 0 if victory else consecutiveLosses + 1
+                bet = initialBet if victory else bet * 2 
+
             numBets = numBets + 1
 
         
         simulation.append( {
             'meta_alcanzada': 'SI' if initialAmount == target else 'QUIEBRA',
-            'num_bets': numBets,
+            'numero_apuestas': numBets,
             'jugadas': plays
         })
 
         plays = []
         numBets = 0
         consecutiveLosses = 0
+        consecutiveWins = 0
         bet = initialBet
         initialAmount = amount
         print(simulation)
         print('\n\n')
         input()
+        
+    context['simulation'] = simulation
+
+    calculations(context, rounds, initialAmount, target)
 
     return context
 
 
-    print(simulation)
+def calculations(context: dict, rounds: int, initialAmount: int, target: int) -> dict:
+
+    clear_screen()
+
+    victories: int = np.count_nonzero([1 if simulation['meta_alcanzada'] == 'SI' else 0 for simulation in context['simulation']])
+    average = victories / rounds
+    print(f"\nLa probabilidad porcentual de llegar a la meta es de: {average:.0%}")
+    hope: float =  ( (target - initialAmount)*average - initialAmount * (1 - average) )
+    print(f"\nLa ganancia esperada es de: {hope:.2g} u.m.")
+
+    # for index, simulation in enumerate(context['simulation']):
+    #     for play in simulation['jugadas']:
+    #         print(play)
+    #         input()
+
+
+    # xlsx_file = export_excel(context['simulation'])
+    # print(f'\nSe ha guardado la simulación en el excel ubicado en "{xlsx_file}"')
+
     input()
+
+
     return context
+
+
+
+
+def export_excel(resultados: list[dict]) -> str:
+
+    # filename = f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx'
+    filename ='hola.xlsx'
+
+    dataset = []
+    for index, simulation in enumerate(resultados):
+        for play in simulation['jugadas']:
+            dataset.append((index + 1, play['monto_antes'], play['apuesta'], play['rand'],
+                            'sí' if play['victoria'] else 'no', play['nuevo_monto'], simulation['meta_alcanzada']))
+    cols = [
+        'Número de la corrida',
+        'Cantidad antes de jugar',
+        'Monto de la Apuesta',
+        'Número aleatorio',
+        '¿Se ganó el juego?',
+        'Cantidad luego de jugar',
+        '¿Se llegó a la meta?'
+    ]
+
+    print(dataset)
+    input()
+
+    df = pd.DataFrame(dataset, columns=cols)
+    df.to_excel(xlsx_file := f'{filename}', index=False)
+    return xlsx_file
